af7fa35bea4c0387921a8049f779faab2d4aa262
Birhanu <birhbelte@gmail.com>
2023-03-16 12:08:37 -0700

    Hello-printf-done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+	printf("with proper grammer, but the outcome is a piece of art \n");
+	return (0);
+	}
