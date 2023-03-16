3ae331e0595bde4f0978488ce1fe3d9fb3e862e2
Birhanu <birhbelte@gmail.com>
2023-03-16 13:14:53 -0700

    unix syetem

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+	retun (1);
+	}
