6595e7be853ae54b21d712bb94354aaf9e7e9eb6
Birhanu <birhbelte@gmail.com>
2023-03-16 12:38:54 -0700

    program returing size of different data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+	double d;
+	short int si;
+	long int li;
+	long long int lli;
+
+	printf("Size of int:%zu byte(s)\n", sizeof(a));
+	printf("Size of float:%zu byte(s)\n", sizeof(b));
+	printf("Size of char:%zu byte(s)\n", sizeof(c));
+	printf("Size of double:%zu byte(s)\n", sizeof(d));
+	printf("Size of short int:%zu byte(s)\n", sizeof(si));
+	printf("Size of long int:%zu byte(s)\n", sizeof(li));
+	printf("Size of long long int:%zu byte(s)\n", sizeof(lli));
+	return (0);
+	}
