f6d93e1344de05f43c31cdaddb1072723b1108ef
Birhanu <birhbelte@gmail.com>
2023-03-16 04:15:13 -0700

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - program prints programming is like building a multilingual pizzle.
- *
+ * Description : this is the first c function
  * Return : 0 -it returns zero if it runs succesfully.
 */
 int main(void)
