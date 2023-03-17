95a475ffd2e6101a1131c2331a4cc8f39ab7e0c6
Birhanu <birhbelte@gmail.com>
2023-03-17 15:44:21 -0700

    program for possible combinations of single-digit number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
