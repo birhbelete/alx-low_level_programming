41cb6f190eba95ed9c1dfc58e4217181dad558d4
Birhanu <birhbelte@gmail.com>
2023-03-17 15:09:06 -0700

    lower-case_letters_in_reverse_0rder

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	int x = 122;
+
+	while (x >= 97)
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
