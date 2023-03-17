71e5d080bfdd62ac2014a1b81fdde857f0384da7
Birhanu <birhbelte@gmail.com>
2023-03-17 14:04:46 -0700

    printing_loewer-case-letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char i = 'a';
-	
-	while (i <= 'z')
+	int i = 97;
+
+	while (i <= 122)
 	{
-		putchar("%c", i);
+		putchar(i);
 		i++;
 	}
 	putchar("\n");
 	return (0);
 }
-	
