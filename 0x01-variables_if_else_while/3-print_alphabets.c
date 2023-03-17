914bef82ec65086607bf53f9378ef1556e030179
Birhanu <birhbelte@gmail.com>
2023-03-17 14:20:19 -0700

    lower-and-upper-case-letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	int i = 97;
+	int y = 65;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (i <= 90)
+	{
+		putchar(y);
+		y++;
+	}
+		putchar('\n');
+	return (0);
+}
