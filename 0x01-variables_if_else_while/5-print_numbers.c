6ea97ddcb42255d2918ffb503726ca3e10e01a60
Birhanu <birhbelte@gmail.com>
2023-03-17 14:44:40 -0700

    numbers from-1-to_9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - a function that prints a piece of art
+ *Description: main function performs printf function
+ *Return: zero for sucess
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '1' ; x <= '9' ; x++)
+		putchar(x);
+	return (0);
+}
