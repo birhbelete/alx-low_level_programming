ffd9354ce31190479e56757f7f41adfebcdb6d06
Birhanu <birhbelte@gmail.com>
2023-03-21 14:22:18 -0700

    print-last-digit-madified

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,24 @@
 #include "main.h"
-/**
- * print_last_digit - function that prints the last digit of a number
- * @i: function argument
- * Return:last digit
- */
-int print_last_digit(int i)
-{
-	int j;
 
-	j = i % 10;
-	if (j < 0)
-	{
-		_putchar (j + 48);
-		return (-1);
-	}
-	else
-	{
-		_putchar (j + 48);
-		return (1);
-	}
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
+
