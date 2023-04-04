02f6f20d06aa678bff53e693b5cc45d11ba882ed
Birhanu <birhbelte@gmail.com>
2023-04-03 23:27:30 -0700

    a function that fills the memory address with a constant bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _memset -used to set a memory block
- * @i: starting address of memory to be filled
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *i, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
